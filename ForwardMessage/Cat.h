021428f049d9a87c83b65b3aafe9641ad02c3e39
WangCe <634692517@qq.com>
2016-04-30 00:46:25 +0800

    Merge pull request #2 from HaloWang/master - perfect message forward

--- ForwardMessage/Cat.h
+++ ForwardMessage/Cat.h
@@ -12,4 +12,6 @@
 
 + (void)stoke;
 
+- (void)stoke;
+
 @end
--- ForwardMessage/ViewController.m
+++ ForwardMessage/ViewController.m
@@ -4,7 +4,6 @@
 // ForwardMessage / 消息转发
 
 #import "ViewController.h"
-
 #import "Cat.h"
 
 @interface ViewController ()
@@ -13,13 +12,12 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
+- (IBAction)instanceMethodCalling {
+    [[Cat new] stoke];
+}
+
+- (IBAction)classMethodCalling {
     [Cat stoke];
-    
-    // 输出：
-    // 2016-04-24 00:27:12.823 ForwardMessage[50298:3350384] Cat 没有实现 stoke 方法，并且成功的转成了 touch 方法
 }
 
 @end
